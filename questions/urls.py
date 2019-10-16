79c1a56fd4aa1cb2e800acfee8ec85d4ee3f1fe0
Seon Yang <ergaster91@gmail.com>
2019-10-16 09:50:00 +0900

    Question모델폼 생성, create-GET 요청 완성

--- questions/urls.py
+++ questions/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import path, include
 from . import views
+
 app_name = "questions"
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('create/', views.create, name="create"),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+from .forms import QuestionForm
 # Create your views here.
 
 
 def index(request):
     return render(request, 'questions/index.html')
+
+
+def create(request):
+    if request.method == "POST":
+        pass
+    else:
+        form = QuestionForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'questions/form.html', context)
